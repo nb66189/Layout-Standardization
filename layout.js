b8985eaa85a0c37eec2607c12bc7fae26b009014
Naveen Kumar <naveennkl888@gmail.com>
2022-03-24 22:24:05 +0530

    without scroll change

---
+++ layout.js
@@ -0,0 +1,24 @@
+       var i = 0;
+
+       function changeColor() {
+           var doc = document.getElementsByClassName("footer");
+           var col = ["Green", "blue", "orange", "green", "purple", "black", "brown", "pink"];
+           doc[0].style.color = col[i];
+           console.log(i);
+           i++;
+           if (i > col.length - 1) {
+               i = 0;
+           }
+       }
+       setInterval(changeColor, 5000);
+
+       function getNewColor() {
+           var symbols, color;
+           symbols = "0123456789ABCDEF";
+           color = "#";
+           for (var i = 0; i < 6; i++) {
+               color = color + symbols[Math.floor(Math.random() * 16)];
+           }
+           document.body.style.background = color;
+       }
+       setInterval(getNewColor, 7000);
